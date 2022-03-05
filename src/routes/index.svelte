0003b1629cba7fc034e7f82a40f9785a2cc64b92
frederikhors <41120635+frederikhors@users.noreply.github.com>
2022-03-05 17:02:21 +0100

    repro

---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="test">Click on this test link</a>
+<br /><br />
+<a href="fake">Click on this fake link</a>
